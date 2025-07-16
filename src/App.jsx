5136d961b4e2f7350b8b6d90768357b0f09028a5
오송주 <dhthdwn7920@gmail.com>
2025-07-16 13:13:16 +0900

    fix: 앱 비율 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import React from "react";
 import styled from "styled-components";
-import Buttons from "./components/buttons";
-import Inputs from "./components/inputs";
-import Texts from "./components/text";
 
 function App() {
   return (
     <AppContainer>
-      <MainContent>
-        <Section>
-          <SectionTitle>Buttons</SectionTitle>
-          <Buttons />
-        </Section>
-
-        <Section>
-          <SectionTitle>Inputs</SectionTitle>
-          <Inputs />
-        </Section>
-
-        <Section>
-          <SectionTitle>Texts</SectionTitle>
-          <Texts />
-        </Section>
-      </MainContent>
+      <MainContent></MainContent>
     </AppContainer>
   );
 }
@@ -31,28 +12,16 @@ export default App;
 
 // Styled Components
 const AppContainer = styled.div`
-  width: 420px;
+  width: 394px;
   margin: 0 auto;
   background-color: #ffffff;
-  height: 100vh;
+  height: 852px;
   border: 1px solid #ddd;
   overflow-y: auto;
 `;
 
 const MainContent = styled.div`
-  padding: 24px;
   display: flex;
   flex-direction: column;
   gap: 60px;
-`;
-
-const Section = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-`;
-
-const SectionTitle = styled.h2`
-  font-size: 28px;
-  font-weight: 700;
 `;
