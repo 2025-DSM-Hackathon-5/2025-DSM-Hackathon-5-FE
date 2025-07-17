35472124d59683d46091cae3eb306df9bd6ce0c7
ojinikim <ojinikim@gmail.com>
2025-07-17 15:28:47 +0900

    chore

--- src/router.jsx
+++ src/router.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./page/Login/Login";
 import Signup from "./page/signup/Signup";
 import AIProfile from "./page/AISetting/AIProfile";
-import AISetting from "./page/aiSetting/AISetting";
+import AISetting from "./page/AISetting/AISetting";
 import Chat from "./page/Chat/Chat";
 import Tips from "./page/Tips/Tips";
 import CreateTips from "./page/Tips/CreateTips";
